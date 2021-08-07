eda8ba90d44cacbd1121f46dec268d67d25483b5
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-07 08:01:13 +0000

    Add new coin post handler

--- routes.py
+++ routes.py
@@ -14,8 +14,20 @@ def admin_index():
 
 @app.route("/admin/coin")
 def admin_coin():
-  return render_template("admin_coin.html")
+  return render_template("admin_coin.html", countries=["maa1","maa2"])
 
 @app.route("/admin/coin/<int:id>")
 def admin_coin_id(id):
   return render_template("admin_coin_id.html")
+
+@app.route("/new_coin", methods=["POST"])
+def new_coin():
+  country = request.form["country"]
+  year = request.form["year"]
+  currency = request.form["currency"]
+  value = request.form["value"]
+  name = request.form["name"]
+  description = request.form["description"]
+  material = request.form["material"]
+  print(country)
+  return redirect("/admin/coin")
