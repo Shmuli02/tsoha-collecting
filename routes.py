bdb955e3cdf37e04bf776e0cb4301661d0347076
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-07 07:50:15 +0000

    add admin routes

--- routes.py
+++ routes.py
@@ -7,3 +7,15 @@ from db import db
 @app.route("/")
 def index():
   return render_template("index.html")
+
+@app.route("/admin")
+def admin_index():
+  return render_template("admin_index.html")
+
+@app.route("/admin/coin")
+def admin_coin():
+  return render_template("admin_coin.html")
+
+@app.route("/admin/coin/<int:id>")
+def admin_coin_id(id):
+  return render_template("admin_coin_id.html")
