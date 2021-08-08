a3d51b042944b3ea2957ed48cbfea0e7b9f08af3
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-08 09:03:06 +0000

    Add messages

---
+++ messages.py
@@ -0,0 +1,13 @@
+from db import db
+from flask import session
+
+def get_countries():
+  sql = "SELECT country FROM country"
+  result = db.session.execute(sql)
+  return result.fetchall()
+
+def add_new_coin(name,description,country,value,currency,material):
+  sql = "INSERT INTO coin_data (name,description,country,value,currency,material) VALUES (:name,:description,:country,:value,:currency,:material)"
+  db.session.execute(sql,{"name":name,"description":description,"country":country,"value":value,"currency":currency,"material":material})
+  db.session.commit()
+  return
