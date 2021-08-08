a4b491d651ed105c8f67245c6e4d7e8a3188ce84
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-08 16:11:32 +0000

    update user login

--- users.py
+++ users.py
@@ -4,11 +4,26 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import messages
 
 def login(username,password):
-  session["username"] = username
+  sql = "SELECT id, password FROM users WHERE username=:username"
+  result = db.session.execute(sql, {"username":username})
+  user = result.fetchone()
+  if not user:
+    return False
+  else:
+    hash_value = user.password
+    if check_password_hash(hash_value,password):
+      session["username"] = username
+      return True
+    else:
+      return False
+  
 
-def register(username, password):
-  hash_value = generate_password_hash(password)
-  sql = "INSERT INTO users (username,password,admin) VALUES (:username,:password,0)"
-  db.session.execute(sql,{"username":username,"password":hash_value})
-  db.session.commit()
-  return 
+def register(username, password,password2):
+  if password == password2:
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username,password,admin) VALUES (:username,:password,0)"
+    db.session.execute(sql,{"username":username,"password":hash_value})
+    db.session.commit()
+    return True
+  else:
+    return False
