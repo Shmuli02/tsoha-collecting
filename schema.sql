ddce1624f9599131fc31bb5c036d1713efb12fed
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-08 09:03:27 +0000

    update sql schema

--- schema.sql
+++ schema.sql
@@ -1,30 +1,24 @@
-CREATE TABLE collectibles (
-
-);
-
-CREATE TABLE coin_collection (
-
-);
-
 CREATE TABLE coin_data (
   id SERIAL PRIMARY KEY,
   name TEXT,
   description TEXT,
-  country TEXT REFERENCES country,
+  country INTEGER REFERENCES country,
   value INTEGER,
-  currency TEXT REFERENCES currency,
-  material TEXT REFERENCES material,
-  mintage_amount INTEGER
+  currency INTEGER REFERENCES currency,
+  material INTEGER REFERENCES material,
+  mintage_amount INTEGER DEFAULT NULL,
   public BOOLEAN
 );
 
 CREATE TABLE coin_library (
   id SERIAL PRIMARY KEY,
-  name TEXT
+  name TEXT,
+  public BOOLEAN
 );
 
 CREATE TABLE user_coin_own (
   id SERIAL PRIMARY KEY
+
 );
 
 CREATE TABLE user_coin_wishlist (
@@ -53,3 +47,24 @@ CREATE TABLE material (
   id SERIAL PRIMARY KEY,
   material TEXT
 )
+
+INSERT INTO country (country) VALUES ('Andorra'),('Argentina'),('Austria'),('Belarus'),
+('Belgium'),('Bulgaria'),('Canada'),('Croatia'),('Cuba'),
+('Cyprus'),('Czech Republic'),('Denmark'),('Ecuador'),
+('Estonia'),('Finland'),('France'),('Germany'),('Greece'),
+('Guatemala'),('Hungary'),('Ireland'),('Isle of Man'),
+('Italy'),('Japan'),('Kazakhstan'),('Latvia'),('Lithuania'),
+('Luxembourg'),('Macedonia'),('Malta'),('Mexico'),('Moldova'),
+('Monaco'),('Netherlands'),('Nicaragua'),('Norway'),('Paraguay'),
+('Peru'),('Poland'),('Portugal'),('Romania'),('Russia'),
+('San Marino'),('Serbia'),('Slovakia'),('Slovenia'),('South Korea'),
+('Spain'),('Sweden'),('Switzerland'),('Turkey'),('Ukraine'),
+('United Kingdom'),('USA'),('Vatican City')
+
+INSERT INTO material (material) VALUES ('Gold'),('Silver'),('Copper–Nickel (CuNi)'),
+('Nordic gold (CuZnAl)'),('German silver (CuNiZn)'),
+('Bimetal: silver, gold plating'),('Bimetal: gold, silver'),
+('Bimetal: CuNi, Brass'),('Bimetal: CuNi, nordic gold'),
+('Bimetal: silver, nordic gold'),('Bimetal: silver, niobium'),
+('Nickel'),('Nickel, bronze plating'),('Bronze'),('Copper'),
+('Bimetal: silver, titanium'),('Brass'),('Palladium'),('Platinum')
