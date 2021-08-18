d25b24965af91ef1635e99605b42798cc8707442
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-18 10:33:33 +0000

    updte sql schema

--- schema.sql
+++ schema.sql
@@ -2,12 +2,13 @@ CREATE TABLE coin_data (
   id SERIAL PRIMARY KEY,
   name TEXT,
   description TEXT,
-  country INTEGER REFERENCES country,
+  country_id INTEGER REFERENCES country,
   value INTEGER,
-  currency INTEGER REFERENCES currency,
-  material INTEGER REFERENCES material,
+  currency_id INTEGER REFERENCES currency,
+  material_id INTEGER REFERENCES material,
   mintage_amount INTEGER DEFAULT NULL,
-  public BOOLEAN
+  public BOOLEAN,
+  image_url TEXT
 );
 
 CREATE TABLE users (
@@ -16,10 +17,13 @@ CREATE TABLE users (
   password TEXT.
   admin INTEGER);
 
-CREATE TABLE coin_library (
+CREATE TABLE coin_collection (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  public BOOLEAN
+  description TEXT,
+  public BOOLEAN,
+  coins TEXT,
+  author_id INTEGER REFERENCES users
 );
 
 CREATE TABLE user_coin_own (
