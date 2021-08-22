872e91b3d181fbaf1c817c33bcdb62346132bd3a
Shmuli02 <samuli.kaleva@gmail.com>
2021-08-22 15:59:57 +0000

    update sql schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,38 @@
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT,
+  password TEXT,
+  admin INTEGER
+);
+
+CREATE TABLE coin_collection (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT,
+  public BOOLEAN,
+  coins TEXT,
+  author_id INTEGER REFERENCES users
+);
+
+
+
+CREATE TABLE country (
+  id SERIAL PRIMARY KEY,
+  country TEXT
+);
+
+CREATE TABLE currency (
+  id SERIAL PRIMARY KEY,
+  currency TEXT
+);
+
+CREATE TABLE material (
+  id SERIAL PRIMARY KEY,
+  material TEXT
+);
+
 CREATE TABLE coin_data (
   id SERIAL PRIMARY KEY,
   name TEXT,
@@ -9,21 +44,6 @@ CREATE TABLE coin_data (
   mintage_amount INTEGER DEFAULT NULL,
   public BOOLEAN,
   image_url TEXT
-);
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username TEXT,
-  password TEXT,
-  admin INTEGER);
-
-CREATE TABLE coin_collection (
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  description TEXT,
-  public BOOLEAN,
-  coins TEXT,
-  author_id INTEGER REFERENCES users
 );
 
 CREATE TABLE user_user_own (
@@ -39,34 +59,6 @@ CREATE TABLE user_coin_wishlist (
   user_id INTEGER REFERENCES users
 );
 
-CREATE TABLE country (
-  id SERIAL PRIMARY KEY,
-  country TEXT
-);
-
-CREATE TABLE currency (
-  id SERIAL PRIMARY KEY,
-  currency TEXT
-);
-
-CREATE TABLE material (
-  id SERIAL PRIMARY KEY,
-  material TEXT
-)
-
--- INSERT INTO country (country) VALUES ('Andorra'),('Argentina'),('Austria'),('Belarus'),
--- ('Belgium'),('Bulgaria'),('Canada'),('Croatia'),('Cuba'),
--- ('Cyprus'),('Czech Republic'),('Denmark'),('Ecuador'),
--- ('Estonia'),('Finland'),('France'),('Germany'),('Greece'),
--- ('Guatemala'),('Hungary'),('Ireland'),('Isle of Man'),
--- ('Italy'),('Japan'),('Kazakhstan'),('Latvia'),('Lithuania'),
--- ('Luxembourg'),('Macedonia'),('Malta'),('Mexico'),('Moldova'),
--- ('Monaco'),('Netherlands'),('Nicaragua'),('Norway'),('Paraguay'),
--- ('Peru'),('Poland'),('Portugal'),('Romania'),('Russia'),
--- ('San Marino'),('Serbia'),('Slovakia'),('Slovenia'),('South Korea'),
--- ('Spain'),('Sweden'),('Switzerland'),('Turkey'),('Ukraine'),
--- ('United Kingdom'),('USA'),('Vatican City')
-
 INSERT INTO country (country) VALUES ('Alankomaat'),('Albania'),('Andorra'),
 ('Armenia'),('Azerbaidžan'),('Belgia'),('Bulgaria'),('Espanja'),('Georgia'),
 ('Irlanti'),('Islanti'),('Israel'),('Italia'),('Itävalta'),('Kreikka'),
@@ -75,7 +67,7 @@ INSERT INTO country (country) VALUES ('Alankomaat'),('Albania'),('Andorra'),
 ('Portugali'),('Puola'),('Ranska'),('Romania'),('Ruotsi'),('Saksa'),
 ('San Marino'),('Serbia'),('Slovakia'),('Slovenia'),('Suomi'),('Sveitsi'),
 ('Tanska'),('Tšekki'),('Turkki'),('Ukraina'),('Unkari'),('Valko-Venäjä'),
-('Vatikaani'),('Venäjä'),('Viro'),('Yhdistynyt kuningaskunta')
+('Vatikaani'),('Venäjä'),('Viro'),('Yhdistynyt kuningaskunta');
 
 
 INSERT INTO material (material) VALUES ('Gold'),('Silver'),('Copper–Nickel (CuNi)'),
@@ -84,6 +76,6 @@ INSERT INTO material (material) VALUES ('Gold'),('Silver'),('Copper–Nickel (Cu
 ('Bimetal: CuNi, Brass'),('Bimetal: CuNi, nordic gold'),
 ('Bimetal: silver, nordic gold'),('Bimetal: silver, niobium'),
 ('Nickel'),('Nickel, bronze plating'),('Bronze'),('Copper'),
-('Bimetal: silver, titanium'),('Brass'),('Palladium'),('Platinum')
+('Bimetal: silver, titanium'),('Brass'),('Palladium'),('Platinum');
 
-INSERT INTO currency (currency) VALUES ('Euro')
+INSERT INTO currency (currency) VALUES ('Euro');
