ef2b14b9fd1cd5037685618d263f2cc0083ce456
Shmuli02 <samuli.kaleva@gmail.com>
2021-09-04 06:52:09 +0000

    new coin and collection functions added

--- coins.py
+++ coins.py
@@ -1,6 +1,7 @@
 from db import db
 from flask import session
 import ast
+import users
 
 def get_countries():
   sql = "SELECT id,country FROM country"
@@ -34,7 +35,16 @@ def add_new_coin(name,description,country,value,currency,material,public,img_url
   db.session.commit()
   return
 
-
+def add_own_coin(user_id,coin_id,amount):
+  sql = """
+    INSERT INTO
+      coin_user_own (coin_id,amount,user_id)
+    VALUES
+      (:coin_id,:amount,:user_id)
+  """
+  db.session.execute(sql,{'user_id':user_id,'coin_id':coin_id,'amount':amount})
+  db.session.commit()
+  return
 
 def get_all_coins():
   sql = """
@@ -62,8 +72,31 @@ def get_coin_by_id(id):
   else:
     return coin[0]
 
-def get_users_coins(user_id):
-  pass
+def get_coinid_by_coin_name(coin_name):
+  sql = "SELECT id, name FROM coin_data WHERE name=:name"
+  result = db.session.execute(sql,{"name":coin_name})
+  user = result.fetchall()
+  return user[0][0]
+
+def get_coins_user_own(user_id):
+  sql = """
+  SELECT
+    coin_id,COUNT(amount)
+  FROM
+    coin_user_own
+  WHERE
+    user_id=:user_id
+  GROUP BY
+    coin_id
+  """
+  result = db.session.execute(sql,{'user_id':user_id})
+  coins_user_own = [{'coin_id':col1,'amount':col2} for (col1,col2) in result.fetchall()]
+  coins_user_own_with_coin_data = []
+  for coin in coins_user_own:
+    coin_data = get_coin_by_id(coin['coin_id'])
+    coin_data['amount'] = coin['amount']
+    coins_user_own_with_coin_data.append(coin_data)
+  return coins_user_own_with_coin_data
 
 
 def add_new_collection(name,description,public,collection_coins,author):
@@ -94,8 +127,30 @@ def get_collection_by_id(id):
   if len(collection) == 0:
     return False
   else:
-    coins = ast.literal_eval(collection[0]['coins'])
+    collection = collection[0]
+    user_own_coins_id = []
+    try:
+      if session["username"]:
+        user_id = users.get_userid_by_username(session["username"])
+        user_own_coins = get_coins_user_own(user_id)
+        user_own_coins_id = [coin['id'] for coin in user_own_coins] # käyttäjän omistamien kolikoiden id
+    except:
+      pass
+
+    coins = ast.literal_eval(collection['coins'])
     coins_int = [int(x) for x in coins]
-    coins_list = [get_coin_by_id(id) for id in coins_int]
-    collection[0]['coins'] = coins_list
-    return collection[0]
+    all_collection_coins = [] # Kaikki kokoelman kolikot
+    collection_coins_user_not_own = [] #kolikot jotka käyttäjä ei omista jos kirjautunut
+    collection_coins_user_own = [] # kokoelman kolikoista ne jotka käyttäjä omistaa
+    for id in coins_int:
+      coin = get_coin_by_id(id)
+      all_collection_coins.append(coin)
+      if id in user_own_coins_id:
+        collection_coins_user_own.append(coin)
+      else:
+        collection_coins_user_not_own.append(coin)
+    collection['all_collection_coins'] = all_collection_coins
+    collection['collection_coins_user_own'] = collection_coins_user_own
+    collection['collection_coins_user_not_own'] = collection_coins_user_not_own
+
+    return collection
